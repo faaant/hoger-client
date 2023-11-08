8d7cb13ce4b080f19077579e8e39411a47d19f9c
faaant <illiaeducation@gmail.com>
2023-11-08 19:54:13 +0200

    rename to managment

--- src/app/features/admin-dashboard/constants/dashboard-items.ts
+++ src/app/features/admin-dashboard/constants/dashboard-items.ts
@@ -3,19 +3,19 @@ import { DashboardItem } from '@core/models/dashboard';
 export const DASHBOARD_ITEMS: DashboardItem[] = [
   {
     img: '/assets/accounts-managment.svg',
-    name: 'Account managment',
+    name: 'Accounts management',
     route: '/accounts',
     positions: ['Admin'],
   },
   {
     img: '/assets/room-managment.svg',
-    name: 'Rooms managment',
+    name: 'Rooms management',
     route: '/room-managment',
     positions: ['Admin', 'Receptionist'],
   },
   {
     img: '/assets/cleaning-managment.svg',
-    name: 'Cleaning managment',
+    name: 'Cleaning management',
     route: '/cleaner-tasks',
     positions: ['Admin', 'Cleaner'],
   },
